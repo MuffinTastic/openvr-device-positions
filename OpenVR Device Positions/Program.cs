e4f359bf801624cdf40ce754d006349eb88522c1
MuffinTastic <muffintastic32@gmail.com>
2024-01-31 15:32:08 -0800

    GC every once in a while, separate token for overlay thread

--- OpenVR Device Positions/Program.cs
+++ OpenVR Device Positions/Program.cs
@@ -10,19 +10,37 @@ internal static class Program
     [STAThread]
     static void Main()
     {
-        CancellationTokenSource cts = new();
-        CancellationToken ct = cts.Token;
+        CancellationTokenSource mainCTS = new();
+        CancellationTokenSource overlayCTS = new();
+
+        GCLoop( mainCTS.Token );
 
         Log.RegisterSink( verbose: true, ( text ) => Debug.Write( $"{text}\n" ) );
 
         Log.Text( "Starting OVRDP" );
-        OverlayThread.Start( ct );
+        OverlayThread.Start( overlayCTS.Token );
         
         ApplicationConfiguration.Initialize();
-        Application.Run( new Window( ct ) );
+        Application.Run( new Window( mainCTS.Token ) );
 
         Log.Text( "Quitting OVRDP" );
-        cts.Cancel();
+
+        overlayCTS.Cancel();
         OverlayThread.WaitForStop();
+
+        mainCTS.Cancel();
+    }
+
+    // VR is a tad memory intensive. Despite this being C#
+    // we still want to keep it a little slim, so let's
+    // call GC every once in a while.
+    async static void GCLoop( CancellationToken ct )
+    {
+        while ( !ct.IsCancellationRequested )
+        {
+            // 15 seconds
+            await Task.Delay( 15 * 1000 );
+            GC.Collect();
+        }
     }
 }
