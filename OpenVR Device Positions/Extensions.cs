fc9de95350a9d103f625996536091870e71c89a7
MuffinTastic <muffintastic32@gmail.com>
2024-02-01 12:01:21 -0800

    start on saving positions

---
+++ OpenVR Device Positions/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+using Valve.VR;
+
+namespace OVRDP;
+
+// Adapted from openvr_api.cs 
+public static class HmdMatrix34_tExtensions
+{
+    public static Vector3 GetPosition( this HmdMatrix34_t m )
+    {
+        return new Vector3( m.m3, m.m7, -m.m11 );
+    }
+
+    public static bool IsRotationValid( this HmdMatrix34_t m )
+    {
+        return ( ( m.m2 != 0 || m.m6 != 0 || m.m10 != 0 ) && ( m.m1 != 0 || m.m5 != 0 || m.m9 != 0 ) );
+    }
+
+    public static Quaternion GetRotation( this HmdMatrix34_t m )
+    {
+        if ( m.IsRotationValid() )
+        {
+            float w = MathF.Sqrt( MathF.Max( 0, 1 + m.m0 + m.m5 + m.m10 ) ) / 2;
+            float x = MathF.Sqrt( MathF.Max( 0, 1 + m.m0 - m.m5 - m.m10 ) ) / 2;
+            float y = MathF.Sqrt( MathF.Max( 0, 1 - m.m0 + m.m5 - m.m10 ) ) / 2;
+            float z = MathF.Sqrt( MathF.Max( 0, 1 - m.m0 - m.m5 + m.m10 ) ) / 2;
+
+            CopySign( ref x, -m.m9 - -m.m6 );
+            CopySign( ref y, -m.m2 - -m.m8 );
+            CopySign( ref z, m.m4 - m.m1 );
+
+            return new Quaternion( x, y, z, w );
+        }
+
+        return Quaternion.Identity;
+    }
+
+    private static void CopySign( ref float sizeval, float signval )
+    {
+        if ( signval > 0 != sizeval > 0 )
+            sizeval = -sizeval;
+    }
+}
--- OpenVR Device Positions/OVRManager.cs
+++ OpenVR Device Positions/OVRManager.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using Valve.VR;
 using Veldrid;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace OVRDP;
 
@@ -87,13 +88,40 @@ public static class OVRManager
 
         Log.Text( $"Saving devices:" );
 
-        for ( uint i = 0; i < OpenVR.k_unMaxTrackedDeviceCount; i++ )
+        var origin = saveSettings.CenterOnHMD ?
+            ETrackingUniverseOrigin.TrackingUniverseSeated : ETrackingUniverseOrigin.TrackingUniverseStanding;
+
+        var trackedDevicePoses = new TrackedDevicePose_t[OpenVR.k_unMaxTrackedDeviceCount];
+        OpenVR.System.GetDeviceToAbsoluteTrackingPose( origin, 0.0f, trackedDevicePoses );
+
+        for ( int id = 0; id < OpenVR.k_unMaxTrackedDeviceCount; id++ )
         {
-            var deviceClass = OpenVR.System.GetTrackedDeviceClass( i );
+            var deviceClass = OpenVR.System.GetTrackedDeviceClass( (uint) id );
             if ( !desiredClasses.Contains( deviceClass ) ) continue;
 
-            Log.Text( $"  {deviceClass}" );
+            var pose = trackedDevicePoses[id];
+
+            if ( !pose.bDeviceIsConnected )
+                continue;
+
+            if ( !pose.bPoseIsValid && !saveSettings.SaveInvalidPoses )
+                continue;
+
+            var matrix = pose.mDeviceToAbsoluteTracking;
+            var position = matrix.GetPosition();
+            var rotation = matrix.GetRotation();
+
+            Log.Text( $"    {deviceClass} - {pose.eTrackingResult}" );
+            Log.Text( $"      - Pos:{position}" );
+            Log.Text( $"      - Rot:{rotation}" );
+            if ( GetDeviceInfo( (uint) id, out DeviceInfo info ) )
+            {
+                Log.Text( $"      - {info.HardwareModel}" );
+                Log.Text( $"      - {info.RenderModel}" );
+            }
         }
+
+
 
         Log.Text( "Done" );
     }
@@ -112,5 +140,63 @@ public static class OVRManager
             desiredClasses.Add( ETrackedDeviceClass.GenericTracker );
 
         return desiredClasses;
+    }
+
+    private struct DeviceInfo
+    {
+        public string Manufacturer;
+        public string HardwareModel;
+        public string RenderModel;
+    }
+
+    private static bool GetDeviceInfo( uint deviceID, out DeviceInfo info )
+    {
+        ETrackedPropertyError error = default;
+
+        StringBuilder builder = new();
+        const uint bufferSize = 64;
+
+        info = default;
+
+        OpenVR.System.GetStringTrackedDeviceProperty(
+            deviceID,
+            ETrackedDeviceProperty.Prop_ManufacturerName_String,
+            builder,
+            bufferSize,
+            ref error
+        );
+        if ( error != ETrackedPropertyError.TrackedProp_Success )
+            return false;
+
+        info.Manufacturer = builder.ToString();
+
+
+        OpenVR.System.GetStringTrackedDeviceProperty(
+            deviceID,
+            ETrackedDeviceProperty.Prop_ModelNumber_String,
+            builder,
+            bufferSize,
+            ref error
+        );
+        if ( error != ETrackedPropertyError.TrackedProp_Success )
+            return false;
+
+        info.HardwareModel = builder.ToString();
+
+
+        OpenVR.System.GetStringTrackedDeviceProperty(
+            deviceID,
+            ETrackedDeviceProperty.Prop_RenderModelName_String,
+            builder,
+            bufferSize,
+            ref error
+        );
+        if ( error != ETrackedPropertyError.TrackedProp_Success )
+            return false;
+
+        info.RenderModel = builder.ToString();
+
+
+        return true;
     }
 }
