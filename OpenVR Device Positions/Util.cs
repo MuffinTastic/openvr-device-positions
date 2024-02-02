704d7e15adec87ccc2cb3c72497b3186b07864d6
MuffinTastic <muffintastic32@gmail.com>
2024-02-01 17:55:59 -0800

    Util class with some useful stuff

---
+++ OpenVR Device Positions/Util.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OVRDP;
+
+public static class Util
+{
+    public static string AssetDirectory => Path.Join( GetExecutableDirectory(), "Assets" );
+    public static string OutputDirectory => Path.Join( GetExecutableDirectory(), "Output" );
+
+    public static void EnsureOutputDirectoryExists()
+    {
+        var outputDirectory = OutputDirectory;
+
+        if ( !Directory.Exists( outputDirectory ) )
+            Directory.CreateDirectory( outputDirectory );
+    }
+
+    public static void OpenOutputDirectory()
+    {
+        Log.Text( "Opening output folder" );
+        Process.Start( "explorer.exe", OutputDirectory );
+    }
+
+    private static string GetExecutableDirectory()
+    {
+        var exePath = Assembly.GetExecutingAssembly().Location;
+        return Path.GetDirectoryName( exePath )!;
+    }
+
+    public static void CopyUShorts( IntPtr source, ushort[] destination, int startIndex, int length )
+    {
+        unsafe
+        {
+            var sourcePtr = (ushort*) source;
+            for ( int i = startIndex; i < startIndex + length; ++i )
+            {
+                destination[i] = *sourcePtr++;
+            }
+        }
+    }
+}
